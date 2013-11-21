fd6a690f096bc192496bb2e6faa11a9a8461eaef
JIM111 <jimi_2112@mail.ru>
2013-11-21 09:16:50 +0200

    Update hz.cpp

--- hz.cpp
+++ hz.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main()
-{ int a, b,c,q,w,e;
+{ int a, b,c;
 SetConsoleOutputCP(1251);
 cout<<" Операция на +"<<endl;
 cout<<"Введите значение А "<<endl;
@@ -13,11 +13,18 @@ cin>> b;
 c=a+b;
 cout<<"Ответ"<<c<<endl;
 cout<<" Операция на *"<<endl;
-cout<<"Введите значение C "<<endl;
-cin>>q;
-cout<<"Введите значение D "<<endl;
-cin>>w;
-e=q*w;
-cout<<"Ответ"<<e<<endl;
+cout<<"Введите значение A "<<endl;
+cin>>a;
+cout<<"Введите значение B "<<endl;
+cin>>b;
+c=a*b;
+cout<<"Ответ"<<c<<endl;
+cout<<"Операция на - "<<endl;
+cout<<"Введите значение A "<<endl;
+cin>> a;
+cout<<"Введите значение B "<<endl;
+cin>> b;
+c=a-b;
+cout<<"Ответ"<<c<<endl;
 return 0;
 }
